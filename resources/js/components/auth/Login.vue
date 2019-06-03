67045e77da20b382a173c525cf18dac526c38e2b
MicroDreamIT <shahanur700@gmail.com>
2019-06-04 02:34:16 +0600

    e3

---
+++ resources/js/components/auth/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="login row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">login</div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label>email</label>
+                                <input type="text" v-model="form.email">
+                            </div>
+                            <div class="form-group">
+                                <label>password</label>
+                                <input type="password" v-model="form.password">
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-default">submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+            return {
+                form:{
+                    email:'',
+                    password:''
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
