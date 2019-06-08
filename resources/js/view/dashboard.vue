265403a7a94a01d8e12697cfe0963fa074854459
MicroDreamIT <shahanur700@gmail.com>
2019-06-08 20:11:40 +0600

    e4

--- resources/js/view/dashboard.vue
+++ resources/js/view/dashboard.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
-        I am dashboard
+        <form @submit.prevent="post" class="form-horizontal">
+
+            <div class="form-group">
+                <label>title</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="form-group">
+                <button @submit.prevent="submit" class="btn btn-success">submit</button>
+            </div>
+
+        </form>
     </div>
 </template>
 
 <script>
     export default {
-        name: "dashboard"
+        name: "dashboard",
+        methods:{
+            post(){
+
+            }
+        }
     }
 </script>
 
